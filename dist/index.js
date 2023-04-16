58426bc6f7801b4142a9a24d6abd31e622eecd3e
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-04-16 23:36:17 +0200

    added the search and get breed routes

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,46 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+axios_1.default.defaults.baseURL = "https://api.thecatapi.com/v1";
+axios_1.default.defaults.headers.common["x-api-key"] = process.env.API_KEY;
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hello from Express and TypeScript");
-});
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`App listening on PORT ${port}`));
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server is running hot");
+});
+app.get("/search-bread/:beardName", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { data } = yield (0, axios_1.default)("/breeds");
+    const breadName = req.params.beardName;
+    const filteredBreads = data.filter((breed) => {
+        return breed.name
+            .toLocaleLowerCase()
+            .includes(breadName.toLocaleLowerCase());
+    });
+    const newFilteredBreeds = filteredBreads.map((breed) => {
+        return { name: breed.name, id: breed.id };
+    });
+    res.send(newFilteredBreeds);
+}));
+app.get("/get-bread/:breedId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { data } = yield (0, axios_1.default)(`/breeds/${req.params.breedId}`);
+    res.send(data);
+}));
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(process.env.API_KEY);
+});
--- index.ts
+++ index.ts
@@ -1,12 +1,45 @@
-import express from "express";
-import fetch from "node-fetch";
+import express, { Express, Request, Response } from "express";
+import axios from "axios";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express and TypeScript");
-});
+axios.defaults.baseURL = "https://api.thecatapi.com/v1";
+axios.defaults.headers.common["x-api-key"] = process.env.API_KEY;
 
+const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`App listening on PORT ${port}`));
+type Breed = {
+  id: string;
+  name: string;
+};
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server is running hot");
+});
+
+app.get("/search-bread/:beardName", async (req: Request, res: Response) => {
+  const { data } = await axios<Breed[]>("/breeds");
+  const breadName = req.params.beardName;
+
+  const filteredBreads = data.filter((breed) => {
+    return breed.name
+      .toLocaleLowerCase()
+      .includes(breadName.toLocaleLowerCase());
+  });
+  const newFilteredBreeds = filteredBreads.map((breed) => {
+    return { name: breed.name, id: breed.id };
+  });
+  res.send(newFilteredBreeds);
+});
+
+app.get("/get-bread/:breedId", async (req: Request, res: Response) => {
+  const { data } = await axios<Breed>(`/breeds/${req.params.breedId}`);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(process.env.API_KEY);
+});
