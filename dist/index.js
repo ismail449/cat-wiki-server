31cc93d02670bf7fc7b25f464d271509aecfe72c
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-04-18 03:35:49 +0200

    added the get top ten searched breeds route

--- dist/index.js
+++ dist/index.js
@@ -13,34 +13,102 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
 const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 axios_1.default.defaults.baseURL = "https://api.thecatapi.com/v1";
 axios_1.default.defaults.headers.common["x-api-key"] = process.env.API_KEY;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = process.env.MONGODB_URI || "";
+const client = new mongodb_1.MongoClient(uri, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
+const dbName = "cat_wiki";
+const collectionName = "cat-breeds";
+const isObjectEmpty = (objectName) => {
+    return Object.keys(objectName).length === 0;
+};
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server is running hot");
 });
-app.get("/search-bread/:beardName", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { data } = yield (0, axios_1.default)("/breeds");
-    const breadName = req.params.beardName;
-    const filteredBreads = data.filter((breed) => {
-        return breed.name
-            .toLocaleLowerCase()
-            .includes(breadName.toLocaleLowerCase());
-    });
-    const newFilteredBreeds = filteredBreads.map((breed) => {
-        return { name: breed.name, id: breed.id };
-    });
-    res.send(newFilteredBreeds);
+app.get("/search-breads/:beardName", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { data } = yield (0, axios_1.default)("/breeds");
+        const breadName = req.params.beardName;
+        const filteredBreads = data.filter((breed) => {
+            return breed.name
+                .toLocaleLowerCase()
+                .includes(breadName.toLocaleLowerCase());
+        });
+        if (filteredBreads.length === 0) {
+            res.status(404).send("Breed NOT Found");
+            return;
+        }
+        const newFilteredBreeds = filteredBreads.map((breed) => {
+            return { name: breed.name, id: breed.id };
+        });
+        res.send(newFilteredBreeds);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(404).send(error.message);
+        }
+    }
 }));
 app.get("/get-bread/:breedId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { data } = yield (0, axios_1.default)(`/breeds/${req.params.breedId}`);
-    res.send(data);
+    try {
+        const { data } = yield (0, axios_1.default)(`/breeds/${req.params.breedId}`);
+        if (isObjectEmpty(data)) {
+            res.status(404).send("Breed NOT Found");
+            return;
+        }
+        yield client.connect();
+        const db = yield client.db(dbName);
+        const breedCollection = yield db.collection(collectionName);
+        const breedCount = yield breedCollection.findOne({
+            id: data.id,
+        });
+        if (!breedCount) {
+            yield breedCollection.insertOne(Object.assign(Object.assign({}, data), { searchCount: 1 }));
+        }
+        else {
+            yield breedCollection.updateOne({ id: data.id }, { $set: { searchCount: breedCount.searchCount + 1 } });
+        }
+        yield client.close();
+        res.send(data);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(404).send(error.message);
+        }
+    }
+}));
+app.get("/get-top-ten-searched-breeds", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        const db = yield client.db(dbName);
+        const topTenSearchedBreeds = yield db
+            .collection(collectionName)
+            .find({})
+            .limit(10)
+            .sort({ searchCount: -1 })
+            .toArray();
+        yield client.close();
+        res.send(topTenSearchedBreeds);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(404).send(error.message);
+        }
+    }
 }));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-    console.log(process.env.API_KEY);
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import axios from "axios";
 import dotenv from "dotenv";
 
@@ -7,6 +8,16 @@ dotenv.config();
 axios.defaults.baseURL = "https://api.thecatapi.com/v1";
 axios.defaults.headers.common["x-api-key"] = process.env.API_KEY;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = process.env.MONGODB_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
@@ -14,32 +25,97 @@ type Breed = {
   id: string;
   name: string;
 };
+type BreedCount = {
+  id: string;
+  searchCount: number;
+};
+
+const dbName = "cat_wiki";
+const collectionName = "cat-breeds";
+
+const isObjectEmpty = (objectName: {}) => {
+  return Object.keys(objectName).length === 0;
+};
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server is running hot");
 });
 
-app.get("/search-bread/:beardName", async (req: Request, res: Response) => {
-  const { data } = await axios<Breed[]>("/breeds");
-  const breadName = req.params.beardName;
+app.get("/search-breads/:beardName", async (req: Request, res: Response) => {
+  try {
+    const { data } = await axios<Breed[]>("/breeds");
+    const breadName = req.params.beardName;
 
-  const filteredBreads = data.filter((breed) => {
-    return breed.name
-      .toLocaleLowerCase()
-      .includes(breadName.toLocaleLowerCase());
-  });
-  const newFilteredBreeds = filteredBreads.map((breed) => {
-    return { name: breed.name, id: breed.id };
-  });
-  res.send(newFilteredBreeds);
+    const filteredBreads = data.filter((breed) => {
+      return breed.name
+        .toLocaleLowerCase()
+        .includes(breadName.toLocaleLowerCase());
+    });
+    if (filteredBreads.length === 0) {
+      res.status(404).send("Breed NOT Found");
+      return;
+    }
+    const newFilteredBreeds = filteredBreads.map((breed) => {
+      return { name: breed.name, id: breed.id };
+    });
+    res.send(newFilteredBreeds);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(404).send(error.message);
+    }
+  }
 });
 
 app.get("/get-bread/:breedId", async (req: Request, res: Response) => {
-  const { data } = await axios<Breed>(`/breeds/${req.params.breedId}`);
-  res.send(data);
+  try {
+    const { data } = await axios<Breed>(`/breeds/${req.params.breedId}`);
+    if (isObjectEmpty(data)) {
+      res.status(404).send("Breed NOT Found");
+      return;
+    }
+    await client.connect();
+    const db = await client.db(dbName);
+    const breedCollection = await db.collection(collectionName);
+    const breedCount = await breedCollection.findOne<BreedCount>({
+      id: data.id,
+    });
+    if (!breedCount) {
+      await breedCollection.insertOne({ ...data, searchCount: 1 });
+    } else {
+      await breedCollection.updateOne(
+        { id: data.id },
+        { $set: { searchCount: breedCount.searchCount + 1 } }
+      );
+    }
+    await client.close();
+    res.send(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(404).send(error.message);
+    }
+  }
+});
+
+app.get("/get-top-ten-searched-breeds", async (req: Request, res: Response) => {
+  try {
+    await client.connect();
+    const db = await client.db(dbName);
+    const topTenSearchedBreeds = await db
+      .collection(collectionName)
+      .find<BreedCount>({})
+      .limit(10)
+      .sort({ searchCount: -1 })
+      .toArray();
+
+    await client.close();
+    res.send(topTenSearchedBreeds);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(404).send(error.message);
+    }
+  }
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  console.log(process.env.API_KEY);
 });
